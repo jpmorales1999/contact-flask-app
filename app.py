0c1b64596180e62439831ed9671cfbd5cc1ca966
Juan Pablo Morales <moralestames309@gmail.com>
2022-05-28 23:51:52 -0500

    App Flask Contacts

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+# Mysql Connection
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'python-flask'
+mysql = MySQL(app)
+
+# Settings
+app.secret_key = 'mysecretkey'
+
+@app.route('/')
+def Index():
+	cur = mysql.connection.cursor()
+	cur.execute('SELECT * FROM contacts')
+	data = cur.fetchall()
+	return render_template('index.html', contacts = data)
+
+@app.route('/add_contact', methods=['POST'])
+def add_contact():
+	if request.method == 'POST':
+		fullname = request.form['fullname']
+		phone = request.form['phone']
+		email = request.form['email']
+
+		cur = mysql.connection.cursor()
+		cur.execute('INSERT INTO contacts (fullname, phone,email) VALUES (%s, %s, %s)', (fullname, phone, email))
+		mysql.connection.commit()
+		flash('Contact Added successfully')
+		return redirect(url_for('Index'))
+
+@app.route('/edit/<id>')
+def get_contact(id):
+	cur = mysql.connection.cursor()
+	cur.execute('SELECT * FROM contacts WHERE id = {}'.format(id))
+	data = cur.fetchall()
+	return render_template('edit_contact.html', contact = data[0])
+
+@app.route('/update/<id>', methods=['POST'])
+def edit_contact(id):
+	if request.method == 'POST':
+		fullname = request.form['fullname']
+		phone = request.form['phone']
+		email = request.form['email']
+
+		cur = mysql.connection.cursor()
+		cur.execute('''
+				UPDATE contacts
+				SET fullname = %s,
+					phone = %s,
+					email = %s
+				WHERE id = %s
+			''', (fullname, phone, email, id))
+		mysql.connection.commit()
+		flash('Contact updated successfully')
+		return redirect(url_for('Index'))
+
+@app.route('/delete/<string:id>')
+def delete_contact(id):
+	cur = mysql.connection.cursor()
+	cur.execute('DELETE FROM contacts WHERE id = {}'.format(id))
+	mysql.connection.commit()
+	flash('Contact removed successfully')
+	return redirect(url_for('Index'))
+
+if __name__ == '__main__':
+	app.run(port = 3000, debug = True)
